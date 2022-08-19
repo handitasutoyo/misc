c38845884771337839d2c10a40c12eb9a83ca723
Handita Sutoyo <89432756+handitasutoyo@users.noreply.github.com>
2022-08-19 11:11:23 +0200

    Add files via upload

--- Compressor.py
+++ Compressor.py
@@ -1,9 +1,10 @@
 import numpy as np
+import math
 
 class compressor:
     def __init__(self,mass,freq):
-        self.m=mass #corrected mass flow rate (correlation range from 0.02 to 0.133 kg/s)
-        self.freq=freq #rpm #normalized corrected rpm (maximum 14000 rpm, range from 0.42 to 1)
+        self.m=mass/2.5 #corrected mass flow rate (correlation range from 0.02 to 0.133 kg/s)
+        self.freq=freq*1.2 #rpm #normalized corrected rpm (maximum 14000 rpm, range from 0.42 to 1)
 
 
     def pr(self):
@@ -14,8 +15,8 @@ class compressor:
         g4=-18.851414930402566
         g5=0.5803839900163186
         ratio=g0+g1*self.freq*self.m+g2*self.m**2+g3*self.freq*self.m**2+g4*self.freq**2+g5*((self.freq**3)/self.m)
-        ratio = np.where(ratio < 0, np.nan, ratio)
-        ratio = np.where(ratio > 5, np.nan, ratio)
+        ratio = np.where(ratio < 1, 1, ratio)
+        ratio = np.where(ratio > 5, 5, ratio)
         return ratio
     def efficiency(self):
         f0=8.059894398447188
@@ -28,6 +29,6 @@ class compressor:
         f7=-51.640302738581724
         f8=-0.02334534516666194
         eta=f0*self.freq+f1*self.freq**2+f2*self.freq**3+f3*((self.freq**f4)/self.m)+f5*((self.freq**f6)/self.m)+f7*(self.freq**f8)*self.m
-        eta = np.where(eta < 0, np.nan, eta)
-        eta = np.where(eta > 1, np.nan, eta)
+        eta = np.where(eta < 0.5, np.nan, eta)
+        eta = np.where(eta > 1, 1, eta)
         return eta
--- Gas_Properties.py
+++ Gas_Properties.py
@@ -4,13 +4,27 @@ import math
 
 # z factor calculator from New explicit correlation for the compressibility factor of natural gas: linearized z-factor isotherms
 # DOI 10.1007/s13202-015-0209-3
+
+# Gas heat capacity from as function of specific gravity, pressure, and temperature
+# http://dx.doi.org/10.1016/j.jngse.2014.04.011
+# SG from 0.55-1, temperature 100-1500 K
+
+# input in bar and celcius
 class gas_properties:
+    # input in bar and celcius
     def __init__(self, pressure, temperature, sg):
         self.p = pressure * 14.5038  # psig
-        self.t = (temperature * 9 / 5) + 491.67  # Rankine
+        self.t = (temperature * 9 / 5) + 459.67  # Rankine
         self.sg = sg
 
+        self.Tpc = 169.2 + 349.5 * self.sg - 74 * self.sg ** 2
+        self.Ppc = 756.8 - 131.07 * self.sg - 3.6 * self.sg ** 2
+        self.Tpr = self.t / self.Tpc
+        self.Ppr = self.p / self.Ppc
+
     def z(self):
+        Tpr = self.Tpr
+        Ppr = self.Ppr
         a1 = 0.317842
         a2 = 0.382216
         a3 = -7.768354
@@ -30,11 +44,6 @@ class gas_properties:
         a17 = 176.29
         a18 = 1.88453
         a19 = 3.05921
-        Tpc = 169.2 + 349.5 * self.sg - 74 * self.sg ** 2
-        Ppc = 756.8 - 131.07 * self.sg - 3.6 * self.sg ** 2
-        Tpr = self.t / Tpc
-        Ppr = self.p / Ppc
-
         # correlation uses equation nr. 14
         t = 1 / Tpr
         A = a1 * t * Ppr * (math.exp(a2 * (1 - t) ** 2))
@@ -49,6 +58,51 @@ class gas_properties:
         return z
 
     def gas_dens(self):
-        z=self.z()
-        dens = 28.97 * 0.001 * 100000 * self.sg * (self.p / 14.5038) / (z* 8.314 * (((self.t - 491.67) * (5 / 9)) + 273.15))
-        return dens
+        z = self.z()
+        dens = 28.97 * 0.001 * 100000 * self.sg * (self.p / 14.5038) / (
+                    z * 8.314 * (((self.t - 491.67) * (5 / 9)) + 273.15))  # kg/m3
+        return dens  # kg/m3
+
+    def cp_ideal(self):
+        # SG from 0.55-1, temperature 100-1500 K
+        a1 = -10.9602
+        a2 = 25.9033
+        b1 = 2.1517 * 10 ** -1
+        b2 = -6.8687 * 10 ** -2
+        c1 = -1.3337 * 10 ** -4
+        c2 = 8.6387 * 10 ** -5
+        d1 = 3.1474 * 10 ** -8
+        d2 = -2.8396 * 10 ** -8
+        Tpr = self.Tpr
+        Ppr = self.Ppr
+        t = 1 / Tpr
+        T_Kelvin = self.t * 5 / 9
+        cp_ideal = (a1 * self.sg + a2) + ((b1 * self.sg + b2) * T_Kelvin) + ((c1 * self.sg + c2) * T_Kelvin ** 2) + (
+                    (d1 * self.sg + d2) * T_Kelvin ** 3)
+        return cp_ideal
+
+    def cp_residual(self):
+        # Tpr 1.2-3, Ppr 0.01-15
+        a1 = 4.80828
+        a2 = -4.01563
+        a3 = -0.0700681
+        a4 = 0.0567
+        a5 = 2.36642
+        a6 = -3.82421
+        a7 = 7.71784
+        Tpr = self.Tpr
+        Ppr = self.Ppr
+        t = 1 / Tpr
+        cp_residual = 8.314 * ( ((1 + (a1 * math.exp(a2*(1-t)**2) * Ppr * t )**2 ) / (a7 + a6*(Ppr*t) + (a5*(Ppr*t)**2) + (a4*(Ppr*t)**3))) - ((((a1*(math.exp(a2*(1-t)**2)))**2) * (a3*((Ppr*t)**6)))/((a7 + a6*(Ppr*t) + (a5*(Ppr*t)**2) + (a4*(Ppr*t)**3))**3) ) )
+        return cp_residual
+
+    def cp(self):
+        cpi = self.cp_ideal()
+        cpr = self.cp_residual()
+        cp_real = cpi + cpr
+        return cp_real  # kJ/kmol K or joule/mol K
+
+    def k(self):
+        cp = self.cp()
+        k = cp*1000 / (cp*1000-8134)
+        return k
